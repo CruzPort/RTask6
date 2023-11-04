868da158e7082c7cf8f16238dd59e068009acbcb
CruzPort <125126639+CruzPort@users.noreply.github.com>
2023-11-05 00:38:53 +0530

    Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1 text-white">
-                Prayagraj, UP
+                Rewa, MP
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -18,7 +18,7 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                shreyanshsingh@email.com
+                santraptverma@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
